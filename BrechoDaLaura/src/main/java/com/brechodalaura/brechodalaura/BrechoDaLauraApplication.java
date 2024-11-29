a91f05c7a8f08de77438d223cba0e610469132c2
PedroR2401 <phromao@outlook.com>
2024-11-29 00:09:50 -0300

    Primeiro commit com as dependencias configuradas

---
+++ BrechoDaLaura/src/main/java/com/brechodalaura/brechodalaura/BrechoDaLauraApplication.java
@@ -0,0 +1,13 @@
+package com.brechodalaura.brechodalaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrechoDaLauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrechoDaLauraApplication.class, args);
+	}
+
+}
---
+++ BrechoDaLaura/src/test/java/com/brechodalaura/brechodalaura/BrechoDaLauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brechodalaura.brechodalaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrechoDaLauraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
